348647057c4eeaf6d33fd0a009871e3e789af9a3
Guh-paixao <gustavopaixao.dev@gmail.com>
2024-10-24 18:41:10 -0300

    Add initial project files and configurations

---
+++ lib/main.dart
@@ -0,0 +1,126 @@
+import 'package:expenses/models/transactions.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+
+main() => runApp(ExpensesApp());
+
+class ExpensesApp extends StatelessWidget {
+  const ExpensesApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      localizationsDelegates: const [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate
+      ],
+      supportedLocales: const [Locale('pt', 'BR')],
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  final titleController = TextEditingController();
+  final valueController = TextEditingController();
+
+  final _transactions = [
+    Transactions(
+        id: 't1', title: 'Novo tênis', value: 310.76, date: DateTime.now()),
+    Transactions(
+        id: 't2', title: 'Conta de Luz', value: 211.30, date: DateTime.now()),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    Intl.defaultLocale = 'pt_BR';
+
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Despesas Pessoais'),
+        ),
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Container(
+              width: double.infinity,
+              child: Card(
+                child: Text('Gráfico'),
+              ),
+            ),
+            Column(
+              children: _transactions.map((tr) {
+                return Card(
+                  child: Row(
+                    children: [
+                      Container(
+                        margin: const EdgeInsets.symmetric(
+                            horizontal: 15, vertical: 10),
+                        decoration: BoxDecoration(
+                            border: Border.all(
+                          color: Colors.purple,
+                          width: 2,
+                        )),
+                        padding: const EdgeInsets.all(10),
+                        child: Text(
+                          'R\$ ${tr.value.toStringAsFixed(2)}',
+                          style: const TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                            color: Colors.purple,
+                          ),
+                        ),
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(tr.title,
+                              style: const TextStyle(
+                                  fontSize: 16, fontWeight: FontWeight.bold)),
+                          Text(
+                              DateFormat(
+                                'd MMM y',
+                              ).format(tr.date),
+                              style: TextStyle(color: Colors.grey[600])),
+                        ],
+                      )
+                    ],
+                  ),
+                );
+              }).toList(),
+            ),
+            Card(
+                elevation: 5,
+                child: Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Column(
+                    children: [
+                      TextField(
+                        controller: titleController,
+                        decoration: const InputDecoration(
+                          labelText: 'Título',
+                        ),
+                      ),
+                      TextField(
+                        controller: valueController,
+                        decoration: const InputDecoration(
+                          labelText: 'Valor (R\$)',
+                        ),
+                      ),
+                      TextButton(
+                        onPressed: () {
+                          print(titleController.text);
+                          print(valueController.text);
+                        },
+                        style: TextButton.styleFrom(
+                            foregroundColor: Colors.purple),
+                        child: const Text('Nova Transação'),
+                      )
+                    ],
+                  ),
+                ))
+          ],
+        ));
+  }
+}
